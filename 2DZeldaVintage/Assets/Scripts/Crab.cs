9b9be3b875217dce78485c7d04abb943b6b679c3
Filipe Castanheira <castanhf@mail.gvsu.edu>
2018-12-12 18:14:05 -0500

    blue crabs now moves randomly

--- 2DZeldaVintage/Assets/Scripts/Crab.cs
+++ 2DZeldaVintage/Assets/Scripts/Crab.cs
@@ -6,15 +6,51 @@ public class Crab : MonoBehaviour {
 
     public int health;
     public GameObject particleEffect;
-	// Use this for initialization
-	void Start () {
-		
+    SpriteRenderer spriteRenderer;
+    int direction = 0;
+    float timer = 1.5f; // two seconds
+    public float speed;
+    public Sprite facingUp;
+    public Sprite facingDown;
+    public Sprite facingRight;
+    public Sprite facingLeft;
+    // Use this for initialization
+    void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = facingUp;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+            timer = 1.5f;
+            direction = Random.Range(0, 3); // random number from 0 to 3
+        }
+        Movement();
 	}
+
+    void Movement() {
+        if (direction == 0) {
+            transform.Translate(0, -speed * Time.deltaTime, 0); // moving down with a certain speed
+            spriteRenderer.sprite = facingDown;
+        }
+        else if (direction == 1)
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0); // moving left with a certain speed
+            spriteRenderer.sprite = facingLeft;
+        }
+        else if (direction == 2)
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0); // moving right with a certain speed
+            spriteRenderer.sprite = facingRight;
+        }
+        else if (direction == 3)
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0); // moving down with a certain speed
+            spriteRenderer.sprite = facingUp;
+        }
+    }
 
     void OnTriggerEnter2D(Collider2D col) {
         if(col.gameObject.tag == "Sword") {
